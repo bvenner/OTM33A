65dd6d6f9deb216e14d700511c578026cf7250c6
Bradley Venner <bvenner@proton.me>
2024-12-17 08:12:08 -0700

    update rename

--- R/rename-OTM33A.R
+++ R/rename-OTM33A.R
@@ -8,5 +8,6 @@ rename.OTM33A <- function(dat) {
   setnames(dat,"AirMar.Barometer..mBar.","Pressure")
   setnames(dat,"X3DS.Sonic.Temp","Temp")
   setnames(dat,"AirMar.Wind.Direction..TRUE.","wd2")
+  setnames(dat,"AirMar.Wind.Speed..m.s.","ws2")
   setnames(dat,"AirMar.Air.Temp..oC.","Temp.Alt")
 }
